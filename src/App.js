28e09aa029d9c39d6cf99bb8f4ca00306941d0c2
alishamsian <alishamsian504@gmail.com>
2022-09-06 17:33:21 +0430

    fina

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import { useState } from 'react';
-import './App.css';
-import {Room} from "@material-ui/icons"
-import Map, { Marker } from 'react-map-gl';
+import { useState } from "react";
+import "./App.css";
+import { Room } from "@material-ui/icons";
+import Map, {
+  FullscreenControl,
+  GeolocateControl,
+  Marker,
+  NavigationControl,
+  Popup,
+  ScaleControl,
+} from "react-map-gl";
 
 function App() {
   const [viewState, setViewState] = useState({
-    longitude: -100,
-    latitude: 40,
-    zoom: 3
+    longitude: 17,
+    latitude: 46,
+    zoom: 3.5,
+    bearing: 0,
+    pitch: 0,
   });
   return (
-    <div className='App'>
+    <div className="App" style={{ width: "100%", height: "100vh" }}>
       <Map
         {...viewState}
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-        onMove={evt => setViewState(evt.viewState)}
-        style={{ width: "100vw", height: "100vh" }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        onMove={(evt) => setViewState(evt.viewState)}
+        width="100%"
+        height="100%"
+        transitionDuration="200"
+        mapStyle="mapbox://styles/mapbox/dark-v9"
       >
-        <Marker longitude={2.294694} latitude={48.858093} anchor="bottom" >
-          <Room/>
+        <GeolocateControl position="top-left" />
+        <FullscreenControl position="top-left" />
+        <NavigationControl position="top-left" />
+        <ScaleControl />
+        <Marker longitude={-100} latitude={40} anchor="bottom">
+          <Room style={{ fontSize: viewState.zoom * 7, color: "slateblue" }} />
         </Marker>
+        <Popup
+          longitude={48.858093}
+          latitude={2.294694}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="bottom"
+        >
+          You are here
+        </Popup>
       </Map>
     </div>
   );
